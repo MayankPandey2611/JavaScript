336f0beb56d936420dd457c98c2b0ed5beba8eca
MayankPandey2611 <kitupnd@gmail.com>
2025-04-12 18:30:40 +0530

    form changes

--- FROM/form.js
+++ FROM/form.js
@@ -1,48 +1,55 @@
 let val =()=>{
 
 let username = document.querySelector("#name").value 
-let error = document.querySelector("#errname")
+let phonumber = document.querySelector("#Number").value 
+let email = document.querySelector("#Email").value 
+let userpassword = document.querySelector("#Password").value 
+let cpassword = document.querySelector("#cPassword").value
 
+
+
+let error = document.querySelector("#errname")
+let error1 = document.querySelector("#errnumber")
+let error2 = document.querySelector("#erremail")
+let error3 = document.querySelector("#errcpass")
+let error4 = document.querySelector("#errpass")
 
 
 
 if(username=="" || username==" "){
     error.innerHTML=" please enter your name"
     return false
-};
-
-let phonumber = document.querySelector("#Number").value 
-let error1 = document.querySelector("#errnumber")
+}
 
 
-if(phonumber<10 || phonumber==0){
+
+else if(isNaN(phonumber) ){
     error1.innerHTML=" please enter valid number"
     return false
-
-};
-
-
-let email = document.querySelector("#Email").value 
-let error2 = document.querySelector("#erremail")
+}
 
 
- if(email =="" ){
+else if(phonumber.length!=10){
+error1.innerHTML="please enter 10 digit number"
+return false
+}
+
+else if( !(email.includes("@") && email.includes(".com") )){
     error2.innerHTML=" please enter valid email"
     return false
 
-};
+}
 
+else if(userpassword==""){
+    error4.innerHTML= "please enter password"
+    return false
+}
 
-let userpassword = document.querySelector("#Password").value 
-let cpassword = document.querySelector("#cPassword").value
-let error3 = document.querySelector("#errcpass")
-
-
- if(cpassword!=userpassword){
+else if(cpassword!=userpassword){
     error3.innerHTML=" please enter correct password"
     return false
 
-};
+}
 
 }
 
