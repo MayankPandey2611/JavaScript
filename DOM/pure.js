178a5759b90d7c955fecfc44ec460e758d1e4dc8
MayankPandey2611 <kitupnd@gmail.com>
2025-04-29 17:39:57 +0530

    changes

--- DOM/pure.js
+++ DOM/pure.js
@@ -1,7 +1,42 @@
-//----------------------------------------------EVENT LISTNER-------------------------------------------------------------//
+//----------------------------------------------EVENT LISTENERS-------------------------------------------------------------//
 
-let BTN = document.querySelector("#btn")
+// let BTN = document.querySelector("#btn")
 
-BTN.addEventListener('click' , ()=>{
-    alert("welcome");
-})
+// //1. ONCLICK
+// BTN.addEventListener('click' , ()=>{
+//     alert("welcome");
+// })
+
+
+// let INP = document.querySelector("#inp")
+
+// // 2.ON KEYDOWN
+// // INP.addEventListener('keydown', (e)=>{
+// //     console.log(e.key);
+    
+// // })
+
+
+// //3. ON INPUT
+// INP.addEventListener('input' , ()=>{
+//      console.log(INP.value);
+    
+// })
+
+// // 4. IN WEBSITE
+// INP.addEventListener('input' , ()=>{
+//     let show = document.querySelector("#s")
+//          show.innerHTML=(INP.value);
+        
+//     })
+
+
+// --------------------------------------------------CREATE ELEMENT------------------------------------------//
+
+let  head = document.createElement('h1')
+
+head.innerText = "hello javascript"
+
+let Box = document.querySelector("#box")
+
+Box.appendChild(head)
